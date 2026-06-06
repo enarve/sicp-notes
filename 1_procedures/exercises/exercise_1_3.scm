8170e628734d353096cc901457a9c72fd6c65c18
Nikita <80875524+enarve@users.noreply.github.com>
2026-06-06 14:52:06 +0200

    Update exercise_1_3.scm

--- 1_procedures/exercises/exercise_1_3.scm
+++ 1_procedures/exercises/exercise_1_3.scm
@@ -9,3 +9,11 @@
         ((= (min x y z) y) (+ (square x) (square z)))
         ((= (min x y z) z) (+ (square x) (square y))))
 )
+
+; other version
+(define (func a b c)
+    (define (sqsum a b)
+      (+ (* a a) (* b b)))
+    (cond ((and (< c a) (< c b)) (sqsum a b))
+      ((and (< a c) (< a b)) (sqsum c b))
+      (else (sqsum a c))))
